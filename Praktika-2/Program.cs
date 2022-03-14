93d948393ce023e88185f0307afe1f69d1d620d7
SahilMamedov <sahilim@code.edu.az>
2022-03-14 18:32:57 +0400

    praktika

---
+++ Praktika-2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Praktika_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hesablamag istediyin reqemi yazin");
+            double a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("+ , - , * , / bunlardan birini yazin ");
+            string b = Console.ReadLine();
+            Console.WriteLine("Novbeti reqemi yazin");
+            double c = Convert.ToInt32(Console.ReadLine());
+            
+
+            switch (b)
+            {
+                case"+": Console.WriteLine(a+c);
+                    break;
+                case "-": Console.WriteLine(a-c);
+                    break;
+                case "*": Console.WriteLine(a * c);
+                    break;
+                case "/": Console.WriteLine(a / c);
+                    break;
+            
+
+
+
+        }
+
+
+        }
+    }
+}
+
+      
